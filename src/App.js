c0037d0ba17e6546c1883f348d45ed4cb4c58004
Carlos Mañas <carlos@spacenomads.com>
2019-09-08 20:01:00 +0200

    Add basic solution without state

--- src/App.js
+++ src/App.js
@@ -1,27 +1,74 @@
 import React from 'react';
 import './scss/main.scss';
-import Input from './components/Input';
-import Tries from './components/Tries';
-import Result from './components/Result';
+
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.handleClick = this.handleClick.bind(this)
+
+    this.counter = 0;
+    this.clue = 'Introduce un número y dale a Prueba';
+    this.userNumber = 0;
+    this.randomNumber = this.getRandomNumber(100);
+    console.log('r', this.randomNumber);
+
+    this.getUserUserNumber = this.getUserUserNumber.bind(this);
+    this.checkNumbers = this.checkNumbers.bind(this);
   }
-  handleClick(){
+
+  getRandomNumber(max) {
+    return Math.floor(Math.random() * max);
+  }
+  
+  getUserUserNumber(event) {
+    const value = parseInt(event.currentTarget.value);
+    this.userNumber = value;
+  }
+
+  checkNumbers() {
+    const userNumber = this.userNumber;
+    const randomNumber = this.randomNumber;
+    let counter = this.counter;
+
+    let result = '';
+
+    if (userNumber === randomNumber) {
+      result = 'Has ganado, campeona!';
+    } else if (userNumber > randomNumber) {
+      result = 'Casi, por lo alto';
+      counter++;
+    } else {
+      result = 'Casi, por lo bajo';
+      counter++;
+    }
+
+    this.clue = result;
+    this.counter = counter;
     this.forceUpdate();
-    console.log('Estoy funcionando');
   }
+
   render() {
-    const handleClick = this.handleClick;
     return (
       <div className="App">
         <h1 className = "main__title">
           Adivina el número
         </h1>
-        <Tries />
-        <Input actionToPerform = {handleClick}/>
-        <Result />
+        <p className = "tries_text" >{this.counter}</p>
+        <div className="InputDiv">
+          <label htmlFor = "input" className = "main__subtitle">
+            Introduce aquí tu número.
+          </label>
+          <input
+            type="number"
+            name="input"
+            id="input"
+            className="input"
+            onChange={this.getUserUserNumber}
+          />
+          <button className="btn" onClick={this.checkNumbers}>
+            Prueba
+          </button>
+        </div>
+        <p className="resultText">{this.clue}</p>
       </div>
     );
   }
