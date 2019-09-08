0530de8dd6a3675090a808cf0fbeccc71f43700c
Migara <evamarcod@gmail.com>
2019-09-08 18:20:59 +0200

    Add Local Storage to keep random number

--- src/App.js
+++ src/App.js
@@ -1,27 +1,72 @@
 import React from 'react';
 import './scss/main.scss';
-import Input from './components/Input';
-import Tries from './components/Tries';
-import Result from './components/Result';
+// import Input from './components/Input';
+// import Tries from './components/Tries';
+// import Result from './components/Result';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.handleClick = this.handleClick.bind(this)
+    this.handleClick = this.handleClick.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
+
+  getRandomNumber(max) {
+    const number = Math.ceil(Math.random() * max);
+    console.log(number);
+    localStorage.setItem('RandomNumber', number);
+    return number;
+  }
+
+  handleChange(event){
+    const valueNumber = event.target.value;
+    console.log(valueNumber);
+    localStorage.setItem('ValueNumber', valueNumber)
+  }
+
   handleClick(){
-    this.forceUpdate();
     console.log('Estoy funcionando');
+    const ValueNumber = localStorage.getItem('ValueNumber');
+    const RandomNumber = localStorage.getItem('RandomNumber');
+    if(ValueNumber > RandomNumber){
+      console.log('soy mayor')
+    }
+    else if (ValueNumber < RandomNumber){
+      console.log('soy menor')
+      this.message = 'El número que has introducido es muy pequeño, introduce un número mayor.'
+    }
+    else{
+      console.log('Has acertado campeona')
+    }
   }
+
+
   render() {
+    this.getRandomNumber(100);
     const handleClick = this.handleClick;
+    const handleChange = this.handleChange;
     return (
       <div className="App">
         <h1 className = "main__title">
           Adivina el número
         </h1>
-        <Tries />
-        <Input actionToPerform = {handleClick}/>
-        <Result />
+        <p className = "tries_text" >0</p>    
+        <div className="InputDiv">
+          <label htmlFor = "input" className = "main__subtitle">
+            Introduce aquí tu número.
+          </label>
+          <input
+            type="number"
+            name="input"
+            id="input"
+            className="input js__input"
+            onChange = {handleChange}
+          />
+          <button className="btn js__btn" onClick={handleClick}>
+            Prueba
+          </button>
+        </div>
+        <p className="resultText">Introduce un número y dale a Prueba</p>
+  
       </div>
     );
   }
